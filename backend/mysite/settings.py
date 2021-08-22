7c5e438fe0b303998d0d9076a5786ece1c42b7eb
noname2048 <sungwook.csw@gmail.com>
2021-08-23 00:16:51 +0900

    feat: add django_extensions, modify gitignore

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd
+    "django_extensions",
+    # apps
     "blog.apps.BlogConfig",
 ]
 
@@ -57,7 +60,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [str(BASE_DIR / "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -121,9 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = BASE_DIR / "static"
+STATICFILES_DIRS = [str(BASE_DIR / "static"), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+from django.core.management.utils import get_random_secret_key
