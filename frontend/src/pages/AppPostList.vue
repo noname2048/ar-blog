4b3092473c537e156c6d73f1bd7ff78a35881869
noname2048 <sungwook.csw@gmail.com>
2021-08-25 14:21:13 +0900

    feat: make mainmenu component

--- frontend/src/pages/AppPostList.vue
+++ frontend/src/pages/AppPostList.vue
@@ -1,58 +1,7 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <!--  --><v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app clipped-left color="indigo" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Vue.js - Django web Porgramming</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn text href="/">Home</v-btn>
-      <v-btn text href="/post_list.html">PostList</v-btn>
-      <v-btn text href="/post_detail.html">PostDetail</v-btn>
-
-      <v-spacer></v-spacer>
-
-      <!-- <v-menu offset-y left bottom> -->
-      <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn text v-bind="attrs" v-on="on">
-            <v-icon>mdi-account</v-icon>
-            Anonymous
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item>
-            <v-list-item-title>Login</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>Register</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>Logout</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>Password change</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>>
+    <MainMenu></MainMenu>
 
     <v-main>
       <PostList></PostList>
@@ -64,12 +13,13 @@
 </template>
 
 <script>
+import MainMenu from '../components/MainMenu.vue';
 import PostList from "../components/PostList.vue";
 
 export default {
-  components: { PostList },
+  components: { MainMenu, PostList },
   data: () => ({
-    drawer: null,
+    // drawer: null,
     items: [
       { title: "Dashboard", icon: "mdi-view-dashboard" },
       { title: "Photos", icon: "mdi-image" },
