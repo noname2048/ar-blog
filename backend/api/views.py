d6a635a76900d84a81d2289b541723fc8251d83e
noname2048 <sungwook.csw@gmail.com>
2021-08-28 19:34:57 +0900

    feat: add post-detail feature

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.generic.detail import BaseDetailView
 from django.views.generic.list import BaseListView
 
 from api.views_utill import obj_to_post
@@ -13,3 +14,12 @@ class ApiPostLV(BaseListView):
         qs = context["object_list"]
         post_list = [obj_to_post(obj) for obj in qs]
         return JsonResponse(data=post_list, safe=False, status=200)
+
+
+class ApiPostDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context["object"]
+        post = obj_to_post(obj)
+        return JsonResponse(data=post, safe=True, status=200)
--- frontend/src/components/PostDetail.vue
+++ frontend/src/components/PostDetail.vue
@@ -2,26 +2,18 @@
   <v-container class="grey lighten-5" fluid>
     <v-row align="center" justify="center">
       <v-col cols="12" sm="12">
-        <h1>Post title here</h1>
-        <p>2020-07-10, written by swook</p>
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.modify_dt }}, written by {{ post.owner }}</p>
       </v-col>
     </v-row>
 
     <v-row align="start" justify="center">
       <v-col cols="12" sm="8" lg="7">
         <v-card class="pa-2" outlined tile>
-          <p>
-            I study Vue.js and Django<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            And more Vuetify.
-          </p>
+          <p style="white-space: pre-wrap;">{{ post.content }}</p>
           <div>
             <strong>TAGS:</strong>
-            <v-chip class="ma-2" color="success" outlined>
-              python
-            </v-chip>
-            <v-chip class="ma-2" color="success" outlined>
-              django
-            </v-chip>
+            <v-chip class="ma-2" outlined v-for="(tag, index) in post.tags" :key="index">{{ tag }}</v-chip>
           </div>
         </v-card>
       </v-col>
@@ -56,61 +48,35 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "HelloWorld",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer",
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href:
-          "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
+    post: {},
   }),
+
+  created() {
+    console.log("created()...");
+    this.fetchPostDetail();
+  },
+
+  methods: {
+
+    async fetchPostDetail() {
+      console.log("fetchPostDetail()...");
+
+      try {
+        const res = await axios.get("/api/post/2/");
+        console.log("POST DETAIL GET RES", res);
+        this.post = res.data
+      } catch (err) {
+        console.error("POST DETAIL GET ERR.RESPONSE", err.response);
+        alert(err.response.status + '' + err.response.statusText);
+      }
+
+    }
+  }
 };
 </script>
