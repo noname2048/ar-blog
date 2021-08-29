a25896e82ed23377ef14f399b1d83d0e6d628a52
noname2048 <sungwook.csw@gmail.com>
2021-08-29 18:41:16 +0900

    fix: change list, detail view to template view

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from blog.models import Post
 
 
-class PostLV(ListView):
-    model = Post
-    # template_name = "blog/post_list.html"
+# class PostLV(ListView):
+#     model = Post
+#     template_name = "blog/post_list.html"
 
 
-class PostDV(DetailView):
-    model = Post
+# class PostDV(DetailView):
+#     model = Post
+
+
+class PostListTV(TemplateView):
+    template_name = "blog/post_list.html"
+
+
+class PostDetailTV(TemplateView):
+    template_name = "blog/post_detail.html"
