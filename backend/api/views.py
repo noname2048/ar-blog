06a5d3abed63cd6d6f28e8673a7cba7560594c11
noname2048 <sungwook.csw@gmail.com>
2021-08-31 20:52:04 +0900

    feat: change tag ref from model to get_queryset

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,7 +10,15 @@ from blog.models import Post
 
 
 class ApiPostLV(BaseListView):
-    model = Post
+    # model = Post
+
+    def get_queryset(self):
+        tagname = self.request.GET.get("tagname")
+        if tagname:
+            qs = Post.objects.filter(tags__name=tagname)
+        else:
+            qs = Post.objects.all()
+        return qs
 
     def render_to_response(self, context, **response_kwargs):
         qs = context["object_list"]
